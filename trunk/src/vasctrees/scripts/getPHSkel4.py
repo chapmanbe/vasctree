c8b58ef7fe3a63e6439f78fb762dabea95ea7fb8
brian <brian@localhost>
2014-01-08 13:44:56 -0700

    added getPHSkel4.py which uses the rms rooting that we found to be most effective

---
+++ trunk/src/vasctrees/scripts/getPHSkel4.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from vasctrees.SkeletonGraph import SkeletonGraph
+import vasctrees.SkeletonGraph as skgs
+import sys
+import scipy.ndimage as ndi
+import numpy as np
+import SimpleITK as sitk
+import numpy as np
+
+
+def rootRMS(graph):
+    endp = [(n[0],n[1]['wcrd']) for n in graph.nodes(data=True) if graph.degree(n[0]) == 1]
+    endpa = np.array([n[0] for n in endp])
+    maxk = np.max(endpa[:,2])
+    endp.sort(key=lambda n: np.sqrt((n[0][0]-256)**2+(n[0][1]-0)**2+(n[0][2]-maxk)**2))
+    newRoot = endp[0][0]
+    return newroot
+
+def readImage(fname):
+    img = sitk.ReadImage(fname)
+    aimg = sitk.GetArrayFromImage(img)
+    descriptors = {}
+    descriptors["scale"] = img.GetSpacing()
+    descriptors["origin"] = img.GetOrigin()
+    descriptors["orientation"] = img.GetDirection()
+    return aimg, descriptors
+
+simg,descr = readImage( sys.argv[1] )
+
+try:
+    simg
+except:
+    sys.exit("read of skeleton image failed")
+
+sg = SkeletonGraph(img=simg,
+                   spacing=descr['scale'],
+                   origin=descr['origin'],
+                   orientation=descr['orientation'],
+		   label = sys.argv[1])
+
+##print descr
+#print sg.spacing, sg.origin, sg.orientation
+
+oimg,tmp = readImage( sys.argv[2])
+try:
+    oimg
+except:
+    sys.exit("read of mask image failed")
+sg.setOriginalImage(oimg)
+sg.getGraphsFromSkeleton(verbose=False)
+sg.setLargestGraphToCurrentGraph()
+sg.graphs.keys()
+sg.findEndpointsBifurcations()
+root = rootRMS(sg.cg)
+#sg.viewGraph()
+sg.setRoot(root,key="og_rms")
+sg.traceEndpoints(key='og_rms')
+ogkey = sg.getLargestOrderedGraphKey()
+sg.deleteDegree2Nodes(ogkey)
+sg.prunePaths(ogkey)
+sg.deleteDegree2Nodes(ogkey)
+sg.fitEdges(key=ogkey)
+#print "Define orthogonal planes"
+sg.defineOrthogonalPlanes(ogkey)
+# Now get surface points of original image to map to the centerlines
+dfe = ndi.distance_transform_cdt(oimg)
+points_toMap = np.array(np.nonzero(np.where(dfe==1,1,0))[::-1]).transpose().astype(np.int32)
+#print "mapVoxelsToGraph"
+sg.mapVoxelsToGraph(points_toMap,ogkey)
+#print "assignMappedPointsToPlanes"
+sg.assignMappedPointsToPlanes(ogkey)
+sg.saveCompressedGraphs(sys.argv[3])
